d4bb39b8bb26e5f8670b5fb2bbe87137fe1ee9ef
39351 <agumasek@gmail.com>
2022-09-05 17:33:18 +0200

    commit 6

--- trial 2.py
+++ trial 2.py
@@ -86,6 +86,48 @@ my_string.upper()# to change all to upper case
 my_string.replace('y','a')# change y to a i.e My=Ma
 new_string = "hello hello world"
 new_string.count("hello")# to see repeated strings
+
+# Rate_times.py
+# This program calculates rate and distance problems
+print("Input a rate and a distance")
+rate = float(input("Rate: "))#5
+distance = float(input("Distance: "))#10
+time=(distance/ rate)
+print("Time:", time)#2.0
+#####
+
+#Area.py
+# This program calculates the perimeter and area of a rectangle
+print("Calculate information about a rectangle")
+length = float(input("Length: "))#4
+width = float(input("Width: "))#let's take width 3
+Perimeter=(2 * length + 2 * width)
+print("Area:", length * width)#12.0
+print("Perimeter:",Perimeter)#14.0
+#Temperature.py
+
+# This program converts Fahrenheit to Celsius
+fahr_temp = float(input("Fahrenheit temperature: "))# 40
+celc_temp = (fahr_temp - 32.0) *( 5.0 / 9.0)
+print("Celsius temperature:", celc_temp)# the result will be 4.44444
+##########################################
+##While loops
+a = 0  # FIRST, set the initial value of the variable a to 0(zero).
+while a < 10:  # While the value of the variable a is less than 10 do the following:
+    a = a + 1  # Increase the value of the variable a by 1, as in: a = a + 1!
+    print(a)  # Print to screen what the present value of the variable a is.
+              # REPEAT! until the value of the variable a is equal to 9!? See note.
+    # NOTE:
+    # The value of the variable a will increase by 1
+    # with each repeat, or loop of the 'while statement BLOCK'.
+    # e.g. a = 1 then a = 2 then a = 3 etc. until a = 9 then...
+    # the code will finish adding 1 to a (now a = 10), printing the
+    # result, and then exiting the 'while statement BLOCK'.
+    #              --
+    # While a < 10: |
+    #     a = a + 1 |<--[ The while statement BLOCK ]
+    #     print (a) |
+    #              --
 #conditional statment im python
 x=1
 
